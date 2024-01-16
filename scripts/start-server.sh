a70fa81b46b5cc36f0595641a29947d2404c6bdd
Brandon Clinger <brandon.clinger@gmail.com>
2024-01-16 21:03:09 +0000

    Initial commit

---
+++ scripts/start-server.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+export LD_LIBRARY_PATH=/usr/lib
+export XDG_RUNTIME_DIR=/serverdata/serverfiles
+
+echo "---Preparing Server---"
+chmod -R ${DATA_PERM} ${DATA_DIR}
+
+echo "---Starting Server---"
+if [ -z "${SERVER_PWD}" ]; then
+	if [ "${GAME_STREAM}" == "stable" ]; then
+		cd ${SERVER_DIR}/stable
+		${SERVER_DIR}/stable/server --game-mode ${GAME_MODE} --server-name "${SERVER_NAME}" --mt-enabled --max-players ${MAX_PLAYERS} --headless --allow-lan --port ${GAME_PORT} ${GAME_PARAMS}
+	fi
+	if [ "${GAME_STREAM}" == "PTE" ]; then
+		cd ${SERVER_DIR}/PTE
+		${SERVER_DIR}/PTE/server --game-mode ${GAME_MODE} --server-name "${SERVER_NAME}" --mt-enabled --max-players ${MAX_PLAYERS} --headless --allow-lan --port ${GAME_PORT} ${GAME_PARAMS}
+	fi
+else
+	if [ "${GAME_STREAM}" == "stable" ]; then
+		cd ${SERVER_DIR}/stable
+		${SERVER_DIR}/stable/server --game-mode ${GAME_MODE} --server-name "${SERVER_NAME}" --mt-enabled --max-players ${MAX_PLAYERS} --headless --allow-lan --port ${GAME_PORT} --server-password ${SERVER_PWD} ${GAME_PARAMS}
+	fi
+	if [ "${GAME_STREAM}" == "PTE" ]; then
+		cd ${SERVER_DIR}/PTE
+		${SERVER_DIR}/PTE/server --game-mode ${GAME_MODE} --server-name "${SERVER_NAME}" --mt-enabled --max-players ${MAX_PLAYERS} --headless --allow-lan --port ${GAME_PORT} --server-password ${SERVER_PWD} ${GAME_PARAMS}
+	fi
+fi
